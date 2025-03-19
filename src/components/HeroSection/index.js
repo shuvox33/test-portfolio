c76a91e1cf93afddd51ff2e439a14078d578dde9
Shakib Hasan Shuvo <shuvox33@gmail.com>
2025-03-20 02:18:33 +0600

    Update index.js

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import HeroBgAnimation from '../HeroBgAnimation'
 import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle,SocialMediaIcons,SocialMediaIcon, ResumeButton } from './HeroStyle'
-import HeroImg from '../../images/HeroImage.jpg'
+import HeroImg from '../../images/WhatsApp Image 2025-03-20 at 02.11.53_c2a99ac8.JPG.jpg'
 import Typewriter from 'typewriter-effect';
 import { Bio } from '../../data/constants';
 
